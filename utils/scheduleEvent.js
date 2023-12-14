a4dc139eb3a2b927206657b97e8ca6cfd360fbfa
Jubraj002 <jubraj.dev@scaler.com>
2023-12-15 00:08:07 +0530

    feat: common util for calendar event creation

---
+++ utils/scheduleEvent.js
@@ -0,0 +1,32 @@
+import * as Calendar from 'expo-calendar';
+import { Alert } from 'react-native';
+
+export const createRecurringEvent = async (start_time, end_time) => {
+  try {
+    const calendars = await Calendar.getCalendarsAsync();
+    const defaultCalendar = calendars.find(calendar => calendar.allowsModifications);
+
+    if (!defaultCalendar) {
+      Alert.alert('Error', 'No modifiable calendars available');
+      return;
+    }
+
+    const eventDetails = {
+      title: 'Scaler Study Session',
+      startDate: start_time,
+      endDate: end_time,
+      timeZone: 'GMT',
+      recurrenceRule: {
+        frequency: Calendar.Frequency.WEEKLY,
+        interval: 1, // Repeat weekly
+        occurrence: 10 // Repeat 10 times
+      }
+    };
+
+    const eventId = await Calendar.createEventAsync(defaultCalendar.id, eventDetails);
+    console.log('Event created with ID:', eventId);
+  } catch (error) {
+    console.error('Error creating recurring event', error);
+    Alert.alert('Error', 'Failed to create event: ' + error.message);
+  }
+};
